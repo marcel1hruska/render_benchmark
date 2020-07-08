db70ffb135916a10217a5d4100effd4a093a297d
Marcel Hruska <marcel1hruska@gmail.com>
2020-07-08 12:04:57 -0700

    added fluorescence for ART

--- src/constants.py
+++ src/constants.py
@@ -3,4 +3,4 @@ import os,datetime
 OUTPUT_PATH=str(os.path.join(os.path.dirname(os.path.realpath(__file__)),os.pardir,'outputs-'+str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))))
 REFERENCES_PATH=str(os.path.join(os.path.dirname(os.path.realpath(__file__)),os.pardir,'data','references'))
 RENDERERS=['mitsuba2','ART']
-TEST_CASES=['reflectance','spectral_accuracy','polarization']
+TEST_CASES=['reflectance','spectral_accuracy','polarization','fluorescence']
